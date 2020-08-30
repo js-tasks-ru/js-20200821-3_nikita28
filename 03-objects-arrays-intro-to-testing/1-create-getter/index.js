e5c9d037d30d31759192e4be3bc480d8afa12d74
Nikita7795 <nikson.7795@mail.ru>
2020-08-30 22:10:31 +0300

    3st task

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    const arr = path.split('.')
+    return function (obj) {
+        let objResult = { ...obj }
+        for (const key of arr) {
+            objResult = objResult[key]
+            if (objResult === undefined) {
+                return undefined
+            }
+        }
+        return objResult
+    }
 }
--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -4,6 +4,23 @@
  * @param {number} size - the allowed size of consecutive identical symbols
  * @returns {string} - the new string without extra symbols according passed size
  */
-export function trimSymbols(string, size) {
-
+export function trimSymbols(string, size = 3) {
+    let countString = 0
+    const result = []
+    const str1 = string.split('')
+    if(size === 0){
+        return result.join('')
+    }
+    for (let i = 0; i < string.length; i++) {
+        if (str1[i] === str1[i - 1] && countString < size) {
+            ++countString
+            result.push(str1[i])
+        }
+        else if (str1[i] != str1[i - 1]) {
+            result.push(str1[i])
+            countString = 1
+        }
+    }
+    return result.join('')
 }
+
