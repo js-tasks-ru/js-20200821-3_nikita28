f414086b8acf7753ff6af64944a4a9adce79ec7b
Nikita7795 <nikson.7795@mail.ru>
2020-09-08 13:45:29 +0300

    5st task

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,68 @@
 export default class NotificationMessage {
 
+    element;
+    timer;
+    static checkShow = false;
+
+    constructor(message, { duration = 0, type = '' } = {}) {
+        this.message = message
+        this.duration = duration
+        this.type = type
+
+        this.render()
+    }
+
+    setShowed(showed) {
+        NotificationMessage.checkShow = showed;
+    }
+
+
+    getTimer() {
+        return (this.duration / 1000)
+    }
+
+    getTеmplate() {
+        return `
+            <div class="notification ${this.type}" style="--value:${this.getTimer()}s">
+                <div class="timer"></div>
+                <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+                </div>
+            </div>`
+    }
+
+    render() {
+        const element = document.createElement('div')
+        element.innerHTML = this.getTеmplate();
+        this.element = element.firstElementChild
+    }
+
+    show(target = document.body) {
+        target.append(this.element);
+        if (NotificationMessage.checkShow) {
+            clearTimeout(this.timer);
+            this.remove();
+        }
+        this.setShowed(true);
+        this.timerAdd();
+    }
+
+    timerAdd() {
+        this.timer = setTimeout(() => {
+            this.remove();
+            this.setShowed(false);
+        }, this.duration);
+    }
+
+    remove() {
+        this.element.remove()
+    }
+
+    destroy() {
+        this.remove()
+    }
 }
+
