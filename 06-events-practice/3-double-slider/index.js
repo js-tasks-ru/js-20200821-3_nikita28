94dcef2d24fda06388107ffad4316100d1c67e9d
Nikita7795 <nikson.7795@mail.ru>
2020-09-18 16:03:53 +0300

    'doubleSlider'

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -1,3 +1,81 @@
+
 export default class DoubleSlider {
+    element;
+    subElements
+    seleceted = { 
+        from : 0,
+        to : 0 
+    }
+
+    constructor({ min = 100,
+        max = 200,
+        selected = {},
+        formatValue = value => '$' + value
+    } = {}){
+
+        this.min = min
+        this.max = max
+        this.selected = selected
+        this.formatValue = formatValue
+
+        this.render()
+        this.initEventListeners()
+    }
+
+    render(){
+        const element = document.createElement('div')
+        element.innerHTML = this.template()
+        this.element = document.firstElementChild
+
+        this.subElements = this.getSubElements(element)
+    }
+
+    template(){
+        return `
+        <div class="range-slider">
+    <span> {this.formatValue(this.min)}</span>
+    <div class="range-slider__inner">
+      <span class="range-slider__progress" style="left: 0%; right: 0%"></span>
+      <span class="range-slider__thumb-left" style="left: 0%"></span>
+      <span class="range-slider__thumb-right" style="right: 0%"></span>
+    </div>
+    <span>{this.formatValue(this.max)}</span>
+  </div>
+  `
+    }
+
+    initEventListeners(){
+        const {thumbleft , thumbRight} = this.subElements
+
+        thumbleft.addEventListener('pointerdown' ,event => this.mouseDown(event))
+        thumbRight.addEventListener('pointerdown' ,event => this.mouseDown(event))
+    }
+
+    mouseDown(){
+        
+
+    }
+
+    
+
+
+
+    getSubElements(element) {
+        const elements = element.querySelectorAll('[data-element]');
+    
+        return [...elements].reduce((accum, subElement) => {
+          accum[subElement.dataset.element] = subElement;
+    
+          return accum;
+        }, {});
+      }
+
+      remove(){
+          this.element.remove()
+      }
+
+      destroy(){
+          this.remove()
+      }
 
 }
